84da69ddb3e1629bf813bd1097d76b4e6f4bd564
Facundo Petre <fpetre@altimetrik.com>
2019-07-30 10:46:35 -0300

    Maybe we dont ned to manage state with contxts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Sheet from "./components/resume/Sheet";
 import SideBar from "./components/resume/SideBar";
 import SkillsSlider from "./components/resume/SkillsSlider";
 import Form from "./components/form/Form";
+import { ResumeData } from "./context/interfaces";
+import styled from "styled-components";
+
+const NameAndJobTitleContainer = styled.div`
+  padding: 1.5em;
+  text-align: left;
+`;
 
 const App: React.FC = () => {
+  const [formData, setFormData] = useState<ResumeData>({
+    fullName: "",
+    jobTitle: "",
+    email: "",
+    adress: ""
+  });
+
+  const handleChange = (name: keyof ResumeData) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData({ ...formData, [name]: event.target.value });
+  };
   return (
     <div className="App">
       <Sheet bgColor="red">
         <SideBar bgColor="blue" width={35}>
+          <h4>{formData.email}</h4>
           <SkillsSlider skill="Eat mandarines" percent={100} />
         </SideBar>
+        <NameAndJobTitleContainer>
+          <h1>{formData.fullName}</h1>
+          <h4>{formData.jobTitle}</h4>
+        </NameAndJobTitleContainer>
       </Sheet>
-      <Form />
+      <Form formData={formData} handleChange={handleChange} />
     </div>
   );
 };
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,34 +1,14 @@
 import React from "react";
-import clsx from "clsx";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
-import Input from "./Input";
-
-const currencies = [
-  {
-    value: "USD",
-    label: "$"
-  },
-  {
-    value: "EUR",
-    label: "€"
-  },
-  {
-    value: "BTC",
-    label: "฿"
-  },
-  {
-    value: "JPY",
-    label: "¥"
-  }
-];
+import { ResumeData } from "../../context/interfaces";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      display: "flex",
-      flexWrap: "wrap"
+      display: "block",
+      width: "50%"
     },
     textField: {
       marginLeft: theme.spacing(1),
@@ -39,129 +19,70 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     menu: {
       width: 200
+    },
+    button: {
+      margin: theme.spacing(1)
     }
   })
 );
 
-interface State {
-  name: string;
-  age: string;
-  multiline: string;
-  currency: string;
-}
-
-export default function OutlinedTextFields() {
+export default function OutlinedTextFields(props: {
+  formData: ResumeData;
+  handleChange: (e: any) => any;
+}) {
   const classes = useStyles();
-  const [values, setValues] = React.useState<State>({
-    name: "Cat in the Hat",
-    age: "",
-    multiline: "Controlled",
-    currency: "EUR"
-  });
 
-  const handleChange = (name: keyof State) => (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setValues({ ...values, [name]: event.target.value });
-  };
+  const {
+    formData: { fullName, jobTitle, email, adress },
+    handleChange
+  } = props;
 
   return (
-    <form
-      className={classes.container}
-      noValidate
-      autoComplete="off"
-      style={{ width: "50%" }}
-    >
-      <TextField
-        label="Name"
-        className={classes.textField}
-        value={values.name}
-        onChange={handleChange("name")}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-uncontrolled"
-        label="Uncontrolled"
-        defaultValue="foo"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        required
-        id="outlined-required"
-        label="Required"
-        defaultValue="Hello World"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        error
-        id="outlined-error"
-        label="Error"
-        defaultValue="Hello World"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        disabled
-        id="outlined-disabled"
-        label="Disabled"
-        defaultValue="Hello World"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-email-input"
-        label="Email"
-        className={classes.textField}
-        type="email"
-        name="email"
-        autoComplete="email"
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-password-input"
-        label="Password"
-        className={classes.textField}
-        type="password"
-        autoComplete="current-password"
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-read-only-input"
-        label="Read Only"
-        defaultValue="Hello World"
-        className={classes.textField}
-        margin="normal"
-        InputProps={{
-          readOnly: true
-        }}
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-dense"
-        label="Dense"
-        className={clsx(classes.textField, classes.dense)}
-        margin="dense"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-dense-multiline"
-        label="Dense multiline"
-        className={clsx(classes.textField, classes.dense)}
-        margin="dense"
-        variant="outlined"
-        multiline
-        rowsMax="4"
-      />
-      <TextField
+    <form className={classes.container} noValidate autoComplete="off">
+      <div>
+        <TextField
+          label="Full Name"
+          className={classes.textField}
+          value={fullName}
+          onChange={handleChange("fullName")}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="jobTitle"
+          className={classes.textField}
+          value={jobTitle}
+          onChange={handleChange("jobTitle")}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-email-input"
+          label="Email"
+          className={classes.textField}
+          value={email}
+          onChange={handleChange("email")}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          label="adress"
+          className={classes.textField}
+          value={adress}
+          onChange={handleChange("adress")}
+          margin="normal"
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <Button variant="contained" color="primary" className={classes.button}>
+          + Experience
+        </Button>
+      </div>
+      {/* <TextField
         id="outlined-multiline-flexible"
         label="Multiline"
         multiline
@@ -172,130 +93,7 @@ export default function OutlinedTextFields() {
         margin="normal"
         helperText="hello"
         variant="outlined"
-      />
-      <TextField
-        id="outlined-multiline-static"
-        label="Multiline"
-        multiline
-        rows="4"
-        defaultValue="Default Value"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-helperText"
-        label="Helper text"
-        defaultValue="Default Value"
-        className={classes.textField}
-        helperText="Some important text"
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-with-placeholder"
-        label="With placeholder"
-        placeholder="Placeholder"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-textarea"
-        label="Multiline Placeholder"
-        placeholder="Placeholder"
-        multiline
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-number"
-        label="Number"
-        value={values.age}
-        onChange={handleChange("age")}
-        type="number"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true
-        }}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-search"
-        label="Search field"
-        type="search"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-select-currency"
-        select
-        label="Select"
-        className={classes.textField}
-        value={values.currency}
-        onChange={handleChange("currency")}
-        SelectProps={{
-          MenuProps: {
-            className: classes.menu
-          }
-        }}
-        helperText="Please select your currency"
-        margin="normal"
-        variant="outlined"
-      >
-        {currencies.map(option => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </TextField>
-      <TextField
-        id="outlined-select-currency-native"
-        select
-        label="Native select"
-        className={classes.textField}
-        value={values.currency}
-        onChange={handleChange("currency")}
-        SelectProps={{
-          native: true,
-          MenuProps: {
-            className: classes.menu
-          }
-        }}
-        helperText="Please select your currency"
-        margin="normal"
-        variant="outlined"
-      >
-        {currencies.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </TextField>
-      <TextField
-        id="outlined-full-width"
-        label="Label"
-        style={{ margin: 8 }}
-        placeholder="Placeholder"
-        helperText="Full width!"
-        fullWidth
-        margin="normal"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <TextField
-        id="outlined-bare"
-        className={classes.textField}
-        defaultValue="Bare"
-        margin="normal"
-        variant="outlined"
-        inputProps={{ "aria-label": "bare" }}
-      />
+      /> */}
     </form>
   );
 }
