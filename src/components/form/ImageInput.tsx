51691083e1b6e8911d8af3eea8cf8125206ab3c9
Facundo Petre <fpetre@altimetrik.com>
2019-07-31 10:37:37 -0300

    Changes on ImageInput component

--- src/components/form/ImageInput.tsx
+++ src/components/form/ImageInput.tsx
@@ -1,33 +1,60 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Slider from "@material-ui/core/Slider";
+import { Typography, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1)
+    },
+    button: {
+      margin: theme.spacing(1)
+    },
+    sizeSlider: {
+      width: 250
+    },
+    container: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }
+  })
+);
 
 const ImageInput = (props: any) => {
-  const { handleImageChange } = props;
-  //   const handleImageChange = (e: any) => {
-  //     e.preventDefault();
-
-  //     let reader = new FileReader();
-  //     let file = e.target.files[0];
-
-  //     reader.onloadend = () => {
-  //       this.setState({
-  //         file: file,
-  //         imagePreviewUrl: reader.result
-  //       });
-  //     };
-
-  //     reader.readAsDataURL(file);
-  //   };
+  const { handleImageChange, size, setSize } = props;
+  const classes = useStyles();
   return (
-    <TextField
-      //   label="Picture"
-      //   className={classes.textField}
-      //   value={experienceData.position}
-      onChange={handleImageChange}
-      type="file"
-      margin="normal"
-      variant="outlined"
-    />
+    <div className={classes.container}>
+      <input
+        accept="image/*"
+        style={{ display: "none" }}
+        id="raised-button-file"
+        multiple
+        type="file"
+        onChange={handleImageChange}
+      />
+      <label htmlFor="raised-button-file">
+        <Button variant="contained" component="span" className={classes.button}>
+          Add Picture
+        </Button>
+      </label>
+      <div style={{ marginLeft: "1em" }}>
+        <Typography id="continuous-slider" gutterBottom>
+          Picture size
+        </Typography>
+        <Grid className={classes.sizeSlider}>
+          <Slider
+            value={size}
+            onChange={setSize}
+            aria-labelledby="continuous-slider"
+          />
+        </Grid>
+      </div>
+    </div>
   );
 };
 
